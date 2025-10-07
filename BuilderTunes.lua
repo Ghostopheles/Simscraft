54b4e38c4d84281b245bf37cef36073b35813a56
Ghostopheles <ghost@ghst.tools>
2025-10-06 23:30:57 -0500

    Add everything

---
+++ BuilderTunes.lua
@@ -0,0 +1,108 @@
+local _, internal = ...;
+
+---@class SoundFile
+---@field Path string
+---@field Duration number
+
+local function MakePath(fileName)
+    return "Interface/AddOns/BuilderTunes/Assets/" .. fileName .. ".mp3";
+end
+
+---@param fileName string
+---@param duration number
+---@return SoundFile
+local function MakeSoundFile(fileName, duration)
+    return {
+        Path = MakePath(fileName),
+        Duration = duration
+    };
+end
+
+local SILENT_SOUND_FILE = MakeSoundFile("Silence", 30);
+
+local SOUND_FILES = {};
+
+for fileName, duration in pairs(internal.SOUND_FILES) do
+    tinsert(SOUND_FILES, MakeSoundFile(fileName, duration))
+end
+
+local LAST_SOUND_INDEX;
+
+local function SelectRandomSoundFile()
+    local idx;
+    repeat
+        idx = random(1, #SOUND_FILES);
+    until idx ~= LAST_SOUND_INDEX;
+
+    LAST_SOUND_INDEX = idx;
+    return SOUND_FILES[idx];
+end
+
+local CURRENT_SOUND_HANDLE;
+
+local function IsPlaying()
+    if not CURRENT_SOUND_HANDLE then
+        return false;
+    end
+
+    return C_Sound.IsPlaying(CURRENT_SOUND_HANDLE);
+end
+
+local StopSong;
+local OnSongEnd;
+
+---@param soundFile SoundFile
+local function PlaySong(soundFile)
+    if IsPlaying() then
+        StopSong();
+    end
+
+    local willPlay, soundHandle = PlaySoundFile(soundFile.Path, "Music");
+    if not willPlay then
+        print("Unable to play sound file: '" .. soundFile.Path .. "'");
+        return;
+    end
+
+    CURRENT_SOUND_HANDLE = soundHandle;
+    C_Timer.After(soundFile.Duration, OnSongEnd);
+
+    PlayMusic(SILENT_SOUND_FILE.Path);
+end
+
+local function PlayRandomSong()
+    local soundFile = SelectRandomSoundFile();
+    PlaySong(soundFile);
+end
+
+---@param fadeTime number?
+function StopSong(fadeTime)
+    if not IsPlaying() then
+        return;
+    end
+
+    StopSound(CURRENT_SOUND_HANDLE, fadeTime or 1.5);
+    StopMusic();
+end
+
+function OnSongEnd()
+    if C_HouseEditor.IsHouseEditorActive() then
+        PlayRandomSong();
+    end
+end
+
+---@param newMode number
+local function OnHouseEditorModeChanged(newMode)
+    if newMode == Enum.HouseEditorMode.None then
+        StopSong();
+    else
+        PlayRandomSong();
+    end
+end
+
+local f = CreateFrame("Frame");
+f:RegisterEvent("HOUSE_EDITOR_MODE_CHANGED");
+f:SetScript("OnEvent", function(self, event, ...)
+    if event == "HOUSE_EDITOR_MODE_CHANGED" then
+        OnHouseEditorModeChanged(...);
+    end
+end);
---
+++ Music.lua
@@ -0,0 +1,17 @@
+--- INSTRUCTIONS
+--- To add your own music:
+---     Copy the music file (mp3 only pls) to the Assets folder inside the addon
+---     Add an entry to the table below using the format
+---     Name = Duration (in seconds)
+
+local _, internal = ...;
+
+internal.SOUND_FILES = {
+    TS4Theme = 134,
+    TestCard4 = 178,
+    SulSul = 187,
+    Beatrix = 214,
+    Breakfast = 179,
+    MeToo = 161,
+    Milan = 197
+};
