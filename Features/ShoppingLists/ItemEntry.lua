aa1d02961ed562a11c23edd31fb4760a467a9bc8
Ghost <ghost@ghst.tools>
2026-09-05 23:15:58 -0500

    Better item quantity text, tooltips, hyperlinks

--- Features/ShoppingLists/ItemEntry.lua
+++ Features/ShoppingLists/ItemEntry.lua
@@ -10,11 +10,22 @@ SimscraftShoppingListItemEntryMixin = {};
 
 function SimscraftShoppingListItemEntryMixin:OnLoad()
 	self.DeleteButton:SetScript("OnClick", function() self:OnDeleteButtonClicked(); end);
+	self.ItemButton:SetScript("OnEnter", function() self:OnItemButtonEnter(); end);
+	self.ItemButton:SetScript("OnLeave", function() self:OnItemButtonLeave(); end);
 
 	self.Name:SetPoint("RIGHT", self.QuantityText, "LEFT", -10, 0);
 	self.Name:SetTextScale(1.2);
 
 	self.QuantityText:SetTextScale(1.2);
+	internal.AddTooltip(self.QuantityText);
+
+	self.InspectButton:SetScript("OnClick", function() self:OnInspectButtonClicked(); end);
+	self.InspectButton.tooltipText = "View this decor item in the housing catalog.";
+	internal.AddTooltip(self.InspectButton);
+end
+
+function SimscraftShoppingListItemEntryMixin:OnShow()
+	self:UpdateOwnedQuantity();
 end
 
 function SimscraftShoppingListItemEntryMixin:OnEnter()
@@ -33,6 +44,45 @@ function SimscraftShoppingListItemEntryMixin:OnLeave()
 	self.FadeOut:Play();
 end
 
+function SimscraftShoppingListItemEntryMixin:OnMouseUp(buttonName)
+	if IsModifiedClick("CHATLINK") then
+		HandleModifiedItemClick(self.Name:GetText());
+	end
+end
+
+function SimscraftShoppingListItemEntryMixin:OnHyperlinkClick(link, text)
+	if IsModifiedClick("CHATLINK") then
+		HandleModifiedItemClick(text);
+	end
+end
+
+function SimscraftShoppingListItemEntryMixin:OnHyperlinkEnter(link, text, region)
+	self:ShowItemTooltip(region, "ANCHOR_CURSOR");
+end
+
+function SimscraftShoppingListItemEntryMixin:OnHyperlinkLeave()
+	self:HideItemTooltip();
+end
+
+function SimscraftShoppingListItemEntryMixin:OnItemButtonEnter()
+	self:ShowItemTooltip(self.ItemButton);
+end
+
+function SimscraftShoppingListItemEntryMixin:OnItemButtonLeave()
+	self:HideItemTooltip();
+end
+
+function SimscraftShoppingListItemEntryMixin:ShowItemTooltip(owner, anchor)
+	anchor = anchor or "ANCHOR_TOPLEFT";
+	GameTooltip:SetOwner(self.ItemButton, "ANCHOR_TOPLEFT");
+	GameTooltip:SetItemByID(self.ItemID);
+	GameTooltip:Show();
+end
+
+function SimscraftShoppingListItemEntryMixin:HideItemTooltip()
+	GameTooltip:Hide();
+end
+
 function SimscraftShoppingListItemEntryMixin:Init(data)
 	local itemID = data.ItemID;
 	self:SetItem(itemID);
@@ -46,6 +96,13 @@ end
 
 function SimscraftShoppingListItemEntryMixin:OnDeleteButtonClicked()
 	Registry:TriggerEvent(Events.SHOPPING_LIST_DELETE_ITEM, self.ItemID);
+end
+
+function SimscraftShoppingListItemEntryMixin:OnInspectButtonClicked()
+	C_AddOns.LoadAddOn("Blizzard_HousingDashboard");
+
+	local catalogEntryInfo = C_HousingCatalog.GetCatalogEntryInfoByItem(self.ItemID);
+	EventRegistry:TriggerEvent("HousingCatalogFrame.OpenToDecorID", catalogEntryInfo.recordID);
 end
 
 function SimscraftShoppingListItemEntryMixin:SetItem(itemID)
@@ -69,18 +126,32 @@ end
 
 function SimscraftShoppingListItemEntryMixin:UpdateOwnedQuantity()
 	local catalogEntryInfo = C_HousingCatalog.GetCatalogEntryInfoByItem(self.ItemID);
-	local owned = catalogEntryInfo.totalNumStored;
-	self.OwnedQuantity = owned;
+	if not catalogEntryInfo then
+		self.StoredQuantity = 0;
+		self.PlacedQuantity = 0;
+		return;
+	end
+	self.StoredQuantity = catalogEntryInfo.totalNumStored;
+	self.PlacedQuantity = catalogEntryInfo.totalNumPlaced;
 end
 
 function SimscraftShoppingListItemEntryMixin:UpdateQuantityText()
-	local owned = self.OwnedQuantity;
+	local owned = self.StoredQuantity + self.PlacedQuantity;
 	local required = self.RequiredQuantity;
 	local text = format("%d/%d", owned, required);
 
 	if owned >= required then
-		text = GRAY_FONT_COLOR:WrapTextInColorCode(text);
+		text = GREEN_FONT_COLOR:WrapTextInColorCode(text);
 	end
 
+	local icon = CreateAtlasMarkup("house-chest-icon", 20, 20);
+	text = format("%s %s", icon, text);
 	self.QuantityText:SetTextToFit(text);
+
+	local requiredFmt = "Required: ";
+	local requiredColor = owned < required and RED_FONT_COLOR or GREEN_FONT_COLOR;
+	requiredFmt = requiredFmt .. requiredColor:WrapTextInColorCode("%d");
+
+	local tooltipText = format("Placed: %d\nStorage: %d\n\n" .. requiredFmt, self.PlacedQuantity, self.StoredQuantity, self.RequiredQuantity);
+	self.QuantityText.tooltipText = WHITE_FONT_COLOR:WrapTextInColorCode(tooltipText);
 end
